6b87f1bb9c081d541a00d282e6704705d4e4d840
Luis Felipe <luisfelrib@gmail.com>
2025-08-10 15:08:10 -0300

    feat: add route for generate pages

--- src/mensagens.js
+++ src/mensagens.js
@@ -4,13 +4,15 @@ const MENSAGENS = [
         id: 1,
         name: "brinde_paulo_bebidas",
         titulo: "[Paulo Bebidas] - Premiado",
-        mensagem: "Parabéns você ganhou um brinde!\n\nVá ao Paulo Bebidas para retirar."
+        mensagem: "Parabéns você ganhou um brinde!\n\nVá ao Paulo Bebidas para retirar.",
+        route: "/premiado",
     },
     {
         id: 2,
         name: "tente_novamente",
         titulo: "[Paulo Bebidas] - Não Premiado",
-        mensagem: "Infelizmente não foi dessa vez, continue tentando."
+        mensagem: "Infelizmente não foi dessa vez, continue tentando.",
+        route: "/nao-premiado",
     }
 ];
 
